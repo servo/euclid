9050bc8f12cdd6da3f6bc02b3c14654ca45d1c7d
Patrick Walton <pcwalton@mimiga.net>
2012-10-25 20:18:14 -0700

    Add missing matrix2d.rs

---
+++ matrix2d.rs
@@ -0,0 +1,37 @@
+use std::cmp::FuzzyEq;
+use num::Num;
+
+pub struct Matrix2D<T:Copy FuzzyEq Num> {
+    m11: T, m12: T,
+    m21: T, m22: T,
+    m31: T, m32: T
+}
+
+pub impl<T:Copy FuzzyEq Num> Matrix2D<T> {
+    static pure fn new(m11: T, m12: T, m21: T, m22: T, m31: T, m32: T) ->
+            Matrix2D<T> {
+        Matrix2D {
+            m11: move m11, m12: move m12,
+            m21: move m21, m22: move m22,
+            m31: move m31, m32: move m32
+        }
+    }
+
+    pure fn translate(x: &T, y: &T) -> Matrix2D<T> {
+        let _0 = num::from_int(0);
+        let _1 = num::from_int(1);
+        let matrix = Matrix2D::new(_1, _0,
+                                   _0, _1,
+                                   *x, *y);
+        return matrix;  // FIXME: This does not multiply yet!!
+    }
+
+    static fn identity() -> Matrix2D<T> {
+        let _0 = num::from_int(0);
+        let _1 = num::from_int(1);
+        return Matrix2D::new(_1, _0,
+                             _0, _1,
+                             _0, _0);
+    }
+}
+
