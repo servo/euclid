fdb6c5ab5ff85dd42b4519ed3f779e08d3538ac1
Ms2ger <Ms2ger@gmail.com>
2016-01-13 13:58:07 +0100

    Fix a build warning in the tests.

--- src/point.rs
+++ src/point.rs
@@ -394,9 +394,9 @@ mod typedpoint2d {
     use scale_factor::ScaleFactor;
 
     #[derive(Debug, Copy, Clone)]
-    enum Mm {}
+    pub enum Mm {}
     #[derive(Debug, Copy, Clone)]
-    enum Cm {}
+    pub enum Cm {}
 
     pub type Point2DMm<T> = TypedPoint2D<Mm, T>;
     pub type Point2DCm<T> = TypedPoint2D<Cm, T>;
