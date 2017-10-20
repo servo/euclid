8b14d3fb57b2cfae4b0f17bbbb45d429d78db8c8
Nicolas Silva <nical@fastmail.com>
2017-10-20 15:25:00 +0200

    Introduce TypedTranslation2D/3D

--- src/lib.rs
+++ src/lib.rs
@@ -78,6 +78,7 @@ pub use vector::{BoolVector2D, BoolVector3D, bvec2, bvec3};
 pub use homogen::HomogeneousVector;
 
 pub use rect::{rect, Rect, TypedRect};
+pub use translation::{TypedTranslation2D, TypedTranslation3D};
 pub use rotation::{Angle, Rotation2D, Rotation3D, TypedRotation2D, TypedRotation3D};
 pub use side_offsets::{SideOffsets2D, TypedSideOffsets2D};
 pub use size::{Size2D, TypedSize2D, size2};
@@ -98,6 +99,7 @@ mod side_offsets;
 mod size;
 mod transform2d;
 mod transform3d;
+mod translation;
 mod trig;
 mod vector;
 
