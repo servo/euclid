ae3c6d6f1a0d6658ee7df319ade9ec36262ae610
psinc <44228825+psincf@users.noreply.github.com>
2020-02-04 15:08:04 +0100

    Add Translation2D to default

--- src/lib.rs
+++ src/lib.rs
@@ -118,6 +118,7 @@ pub mod default {
     pub type Transform3D<T> = super::Transform3D<T, UnknownUnit, UnknownUnit>;
     pub type Rotation2D<T> = super::Rotation2D<T, UnknownUnit, UnknownUnit>;
     pub type Rotation3D<T> = super::Rotation3D<T, UnknownUnit, UnknownUnit>;
+    pub type Translation2D<T> = super::Translation2D<T, UnknownUnit, UnknownUnit>;
     pub type Translation3D<T> = super::Translation3D<T, UnknownUnit, UnknownUnit>;
     pub type Scale<T> = super::Scale<T, UnknownUnit, UnknownUnit>;
     pub type RigidTransform3D<T> = super::RigidTransform3D<T, UnknownUnit, UnknownUnit>;
